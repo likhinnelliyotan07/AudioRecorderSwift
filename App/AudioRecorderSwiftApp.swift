66f02373ebf34898f25c30fe3af703807c438d46
Likhin Nelliyotan <lnelliyotan@gmail.com>
2026-06-25 22:53:13 +0530

    initial commit

---
+++ App/AudioRecorderSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioRecorderSwiftApp.swift
+//  AudioRecorderSwift
+//
+//  Created by Likhin Nelliyotan on 25/06/26.
+//
+
+import SwiftUI
+
+@main
+struct AudioRecorderSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
